919e07d8f760dfaac1e9f00b96f328a9235dd51c
candra julius <candrajulius1@gmail.com>
2020-06-05 18:38:52 +0700

    Menambahkan file form

---
+++ Documents/NetBeansProjects/MyAplikasi/src/FormCandra.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Candra Julius Sinaga
+ */
+public class FormCandra extends JFrame{
+    public static void main(String[] args){
+        JFrame FormCandra = new Login();
+        FormCandra.setTitle("Candra Julius Sinaga");
+        FormCandra.setVisible(true);
+        FormCandra.setSize(400,350);
+        FormCandra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        FormCandra.setLocationRelativeTo(null);
+    }
+    
+}
