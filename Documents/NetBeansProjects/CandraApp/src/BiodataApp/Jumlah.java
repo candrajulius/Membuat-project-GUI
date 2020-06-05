2fad3eb5f191f7fe76ef327722563726815d9689
Candra Julius Sinaga <candrajulius1@gmail.com>
2020-06-05 14:03:36 +0700

    Menambahkan file jumlah.java

---
+++ Documents/NetBeansProjects/CandraApp/src/BiodataApp/Jumlah.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BiodataApp;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Jumlah {
+    public static void main(String[] args) {
+        JFrame candra = new Candra();
+        candra.setTitle("Candra Julius Sinaga");
+        candra.setVisible(true);
+        candra.setSize(400,400);
+        candra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        candra.setLocationRelativeTo(null);
+       
+    }
+}
